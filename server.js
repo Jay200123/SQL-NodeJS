773ae2fcae79faf71098190fcfcbb5d894a10948
Jay200123 <kenken.ayonon@gmail.com>
2025-03-09 19:21:34 +0800

    Add static file serving and custom 404 page to server

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 import express from "express";
 import { envConfig } from "./src/config/index.js";
 import { users } from "./src/routes/index.js";
-envConfig();
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+envConfig();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to the server",
-  });
-});
+app.use(express.static("./public"));
 
 app.use("/api/v1", users);
 
 app.all("*", (req, res) => {
-  res.status(404).json({
-    message: "Route Not Found :(",
-  });
+  res.sendFile(__dirname + "/public/notFound.html");
 });
 
 app.listen(process.env.PORT, () => {
